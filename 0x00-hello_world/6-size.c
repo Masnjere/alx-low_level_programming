3dfad1d89039ce964e1c1ab9c31dcfd7c2481c16
Masnjere <masnjere@gmail.com>
2023-06-15 09:53:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,11 @@
  * Return: 0
  */
 int main(void)
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 {
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
