7dc3d93c0d4c46aa6dd2bc1b383a682f7dff5ed3
Masnjere <masnjere@gmail.com>
2023-08-03 05:10:18 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * binary_to_unit - convert a binary number to unsigned int
- * @b: string containing binary number
+ * binary_to_uint - convert binary no. to unsigned int
+ * @b: string containing binary no.
  *
- * Return: the converted number
+ * Return: the converted no.
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
@@ -20,7 +20,6 @@ unsigned int binary_to_unit(const char *b)
 			return (0);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
 
 	return (dec_val);
 }
